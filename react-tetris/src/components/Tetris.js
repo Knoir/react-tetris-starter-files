e505a7c7c986196456ff5d49934862071c700450
Kyle <kyle.black@my.scitexas.edu>
2019-08-17 08:40:03 -0500

    Continuation of Set up
    Stage.js, StartButton.js, Tetris.js, index.js

--- react-tetris/src/components/Tetris.js
+++ react-tetris/src/components/Tetris.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-const Cell = ({ type }) => (
-    <div>cell</div>
-)
+// Components
 
-export default Cell;
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
+
+
+const Tetris = () => {
+
+    return (
+        <div>
+            <Stage />
+        <aside>
+            <div>
+                <Display text="Score" />
+                <Display text="Rows" />
+                <Display text="Level" />
+            </div>
+            <StartButton />
+        </aside>
+        </div>
+    )
+}
+
+export default Tetris;
