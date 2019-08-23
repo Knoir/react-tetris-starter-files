121559f945db2017c9fe6541c198ba691e6b7752
Kyle <kyle.black@my.scitexas.edu>
2019-08-23 17:22:18 -0500

    Created Hooks; Beginnings of Game Mechanics
    Created hooks for game status, player, and stage. Random tetrominos now appear on the stage after clicking "Start Game"; controls for left, right, up, down now work, though there is an anticipated bug due to not having "collided" properly set up yet.

--- react-tetris/src/components/styles/StyledTetris.js
+++ react-tetris/src/components/styles/StyledTetris.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import bgImage from '../../img/bg.png';
 
-export const StyledTetrisWrapper = styled.div `
+export const StyledTetrisWrapper = styled.div`
     width: 100vw;
     height: 100vh;
     background: url(${bgImage}) #000;
